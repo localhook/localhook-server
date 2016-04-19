3ad8a7da77dd4d2fcfd5199b4f28266f251ac90f
Lucas CHERIFI <lucas@cherifi.info>
2016-04-19 19:49:16 +0200

    update tests

--- src/AppBundle/Tests/Controller/NotificationControllerTest.php
+++ src/AppBundle/Tests/Controller/NotificationControllerTest.php
@@ -11,11 +11,15 @@ class NotificationControllerTest extends WebTestCase
         $client = static::createClient();
 
         $webHooks = ['webhook_1', 'webhook_10'];
-        foreach ($webHooks as $webHook) {
-            $client->request('POST', '/' . $webHook . '/notifications?get_param1=abc&get_param2=def', [
-                'post_param1' => 'ghi',
-                'post_param2' => 'jkl',
-            ]);
+        foreach ($webHooks as $key => $webHook) {
+            $client->request(
+                'POST',
+                '/' . $webHook . '/notifications?get_param' . $key . '=abc&get_param' . ($key + 1) . '=def',
+                [
+                    'post_param' . ($key)     => 'ghi',
+                    'post_param' . ($key + 1) => 'jkl',
+                ]
+            );
             $this->assertEquals(200, $client->getResponse()->getStatusCode());
         }
     }
