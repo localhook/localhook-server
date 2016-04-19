06dfa94ff86f85b33116e4a23ef4499fc50057fe
Lucas CHERIFI <lucas@cherifi.info>
2016-04-19 19:50:51 +0200

    upgrade socket io protocol

--- src/AppBundle/Resources/SocketIo/server.js
+++ src/AppBundle/Resources/SocketIo/server.js
@@ -9,50 +9,74 @@ logger.add(logger.transports.Console, {colorize: true, timestamp: true});
 logger.info('SocketIO > listening on port ' + port);
 
 var channels = {};
+var clients = [];
 
 io.on('connection', function (socket) {
     var nb = 0;
 
-    //logger.info('SocketIO > Connected socket ' + socket.id);
+    logger.info(socket.id + ' > Connected socket');
+    //socket.emit('answer_create_channel', {'status': 'ok'});
+    clients.push(socket.id);
 
     // Server events
 
     socket.on('create_channel', function (message) {
         ++nb;
-        logger.info('Init Channel "' + message.endpoint + '" (' + message.privateKey + ')');
-        channels[message.privateKey] = message.endpoint;
+        logger.info(socket.id + ' > Init Channel "' + message.endpoint + '" (' + message.privateKey + ')');
+        channels[message.endpoint] = {"privateKey": message.privateKey};
+        socket.emit('answer_create_channel', {'status': 'ok'});
     });
 
     socket.on('forward_notification', function (message) {
         ++nb;
-        logger.info('Notification received: ' + JSON.stringify(message));
-        // TODO forward to consumers
+        logger.info(socket.id + ' > Notification received: ' + JSON.stringify(message));
+        logger.info(socket.id + ' > clients: ' + JSON.stringify(clients));
+        socket.broadcast.to(message.webHookEndpoint).emit('forwarded_notification', {
+            "method": message.method,
+            "query": message.query,
+            "request": message.request
+        });
     });
 
     // Client Events
 
     socket.on('retrieve_configuration_from_private_key', function (message) {
         ++nb;
-        logger.info('configuration retrieved: ' + JSON.stringify(message));
-        // TODO forward to consumer
+        var foundWebHook = null;
+        for (var webHook in channels) {
+            // skip loop if the property is from prototype
+            if (!channels.hasOwnProperty(webHook)) continue;
+            if (message.privateKey == channels[webHook].privateKey) {
+                foundWebHook = webHook;
+            }
+        }
+        var response = {'endpoint': foundWebHook};
+        socket.emit('answer_retrieve_configuration_from_private_key', response);
+        logger.info(socket.id + ' > configuration sent for private key ' + message.privateKey + ': ' + JSON.stringify(response));
     });
 
     socket.on('subscribe_channel', function (message) {
         ++nb;
-        logger.info('Channel subscription: ' + JSON.stringify(message));
-        // TODO forward to consumer
+        var channel = message.channel;
+        socket.join(channel);
+        logger.info(socket.id + ' > Channel subscription: ' + channel + ' for client ' + socket.id);
+        socket.emit('answer_subscribe_channel', {'status': 'ok'});
     });
 
     socket.on('unsubscribe_channel', function (message) {
         ++nb;
-        logger.info('Channel unsubscription: ' + JSON.stringify(message));
-        // TODO forward to consumer
+        socket.leave(message.channel);
+        socket.emit('answer_unsubscribe_channel', {'status': 'ok'});
+        logger.info(socket.id + ' > Channel unsubscription: ' + JSON.stringify(message));
+
     });
 
-    // socket.on('disconnect', function () {
-    //     logger.info('SocketIO : Received ' + nb + ' messages');
-    //     logger.info('SocketIO > Disconnected socket ' + socket.id);
-    // });
+    socket.on('disconnect', function () {
+        //logger.info('SocketIO : Received ' + nb + ' messages');
+        var index = clients.indexOf(socket.id);
+        clients.splice(index, 1);
+        logger.info(socket.id + ' > Disconnected socket');
+    });
 });
 
 server.listen(port);
--- src/AppBundle/SocketIoConnector.php
+++ src/AppBundle/SocketIoConnector.php
@@ -3,14 +3,27 @@
 namespace AppBundle;
 
 use AppBundle\Entity\WebHook;
+use Doctrine\ORM\EntityManager;
 use ElephantIO\Client;
 use ElephantIO\Engine\SocketIO\Version1X;
+use Exception;
 use Symfony\Component\HttpFoundation\Request;
 
 class SocketIoConnector
 {
     /** @var Client */
     private $client;
+
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+
+        $this->em = $em;
+    }
 
     /**
      * @return $this
@@ -36,16 +49,90 @@ class SocketIoConnector
     }
 
     /**
+     * @param array $messageKeys
+     *
+     * @return array
+     */
+    private function waitForMessage($messageKeys = [])
+    {
+        while (true) {
+            $r = $this->client->read();
+            if (!empty($r)) {
+                $input = json_decode(substr($r, 2), true);
+                $messageKey = $input[0];
+                if (!count($messageKeys) || in_array($messageKey, array_keys($messageKeys))) {
+                    return $input[1];
+                }
+            }
+        }
+    }
+
+    private function ask($messageKey, $messageContent)
+    {
+        $this->client->emit($messageKey, $messageContent);
+
+        return $this->waitForMessage(['answer_' . $messageKey]);
+    }
+
+    private function emitAndCheck($messageKey, $messageContent)
+    {
+        $result = $this->ask($messageKey, $messageContent);
+
+        if ($result['status'] == 'error') {
+            throw new Exception('Socket IO server failed action with error: ' . $result['message']);
+        }
+    }
+
+    /**
      * @param WebHook $webHook
      *
      * @return $this
+     * @throws Exception
      */
     public function createChannel(WebHook $webHook)
     {
-        $this->client->emit('create_channel', [
+        $this->emitAndCheck('create_channel', [
             'endpoint'   => $webHook->getEndpoint(),
             'privateKey' => $webHook->getPrivateKey(),
         ]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     * @throws Exception
+     */
+    public function initChannels()
+    {
+        $webHooks = $this->em->getRepository('AppBundle:WebHook')->findAll();
+        foreach ($webHooks as $webHook) {
+            $this->createChannel($webHook);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $privateKey
+     *
+     * @return $this
+     * @throws Exception
+     */
+    public function retrieveConfigurationFromPrivateKey($privateKey)
+    {
+        return $this->ask('retrieve_configuration_from_private_key', ['privateKey' => $privateKey]);
+    }
+
+    /**
+     * @param string $channel
+     *
+     * @return $this
+     * @throws Exception
+     */
+    public function subscribeChannel($channel)
+    {
+        $this->emitAndCheck('subscribe_channel', ['channel' => $channel]);
 
         return $this;
     }
@@ -66,5 +153,14 @@ class SocketIoConnector
         ]);
 
         return $this;
+    }
+
+    /**
+     * @return $this
+     * @throws Exception
+     */
+    public function waitForNotification()
+    {
+        return $this->waitForMessage(['forwarded_notification']);
     }
 }
