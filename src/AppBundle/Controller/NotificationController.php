48b09de096a170a821f2d55ecce867e1561141c0
Lucas CHERIFI <lucas@cherifi.info>
2016-04-19 19:20:32 +0200

    Add notification controller

---
+++ src/AppBundle/Controller/NotificationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\WebHook;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class NotificationController extends Controller
+{
+    /**
+     * @Route("{endpoint}/notifications", name="notifications")
+     * @param Request $request
+     * @param string  $endpoint
+     *
+     * @return JsonResponse
+     */
+    public function handleAction(Request $request, $endpoint)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $webHook = $em->getRepository('AppBundle:WebHook')->findOneBy(['endpoint' => $endpoint]);
+        
+        $this->get('socket_io_connector')->ensureConnection()->forwardNotification($webHook, $request);
+
+        return new JsonResponse();
+    }
+}
