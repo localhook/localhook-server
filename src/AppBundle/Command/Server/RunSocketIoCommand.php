da204df78b77172d6b4a6a0aa1d3022a35ff8998
Lucas CHERIFI <lucas@cherifi.info>
2016-04-21 16:13:30 +0200

    update command list

---
+++ src/AppBundle/Command/Server/RunSocketIoCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Command\Server;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+
+class RunSocketIoCommand extends ContainerAwareCommand
+{
+    /** @var SymfonyStyle */
+    private $io;
+
+    protected function configure()
+    {
+        $this
+            ->setName('app:server:run-socket-io')
+            ->setDescription('Start the socket IO server');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $process = new Process('node src/AppBundle/Resources/SocketIo/server.js');
+        $process->setTimeout(null)->start();
+
+        $process->wait(function ($type, $buffer) {
+            if (Process::ERR === $type) {
+                echo 'ERR > ' . $buffer;
+            } else {
+                echo $buffer;
+                if (strpos($buffer, 'listening on port')) {
+                    $this->initChannels();
+                    $this->io->comment('Channels successfully created');
+                }
+            }
+        });
+    }
+
+    private function initChannels()
+    {
+        $this
+            ->getContainer()
+            ->get('socket_io_connector')
+            ->ensureConnection()
+            ->initChannels()
+            ->closeConnection();
+    }
+}
