094200ca799ff4daed4acfb33af6f9b7db8c382b
Lucas CHERIFI <lucas@cherifi.info>
2016-04-21 19:12:16 +0200

    new feature : simulate via HTTP (default) or INTERNAL

--- src/AppBundle/Command/Server/SimulateNotificationCommand.php
+++ src/AppBundle/Command/Server/SimulateNotificationCommand.php
@@ -2,30 +2,75 @@
 
 namespace AppBundle\Command\Server;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request as GuzzleRequest;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Process\Process;
 
 class SimulateNotificationCommand extends ContainerAwareCommand
 {
     /** @var SymfonyStyle */
     private $io;
 
+    /** @var OutputInterface */
+    private $output;
+    
     protected function configure()
     {
         $this
             ->setName('app:server:simulate-notification')
+            ->addOption('mode', null, InputOption::VALUE_OPTIONAL, 'The mode to send notification : HTTP / INTERNAL', 'HTTP')
             ->setDescription('Simulate a notification to the socket IO server');
     }
 
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     * @throws \Exception
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->io = new SymfonyStyle($input, $output);
-        $socketIoConnector = $this->getContainer()->get('socket_io_connector')->ensureConnection();
+        $this->output = $output;
+        $mode = $input->getOption('mode');
+        if ($mode == 'HTTP') {
+            $this->httpExecution();
+        } elseif ($mode == 'INTERNAL') {
+            $this->internalExecution();
+        } else {
+            throw new \Exception('Invalid mode: please set one of HTTP or INTERNAL values.');
+        }
+        $this->output->writeln('Sent!');
+    }
 
+    private function httpExecution()
+    {
+        $client = new Client();
+        $request = new GuzzleRequest('POST', 'http://localhost:8000/webhook_1/notifications?get_param_1=get_value_1&get_param_2=get_value_2', [
+            'json' => [
+                'post_param_1' => 'post_value_1',
+                'post_param_2' => 'post_value_2',
+            ],
+        ]);
+        try {
+            $client->send($request, ['timeout' => 15]);
+        } catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                $this->output->writeln('INVALID RESPONSE CODE: ' . $e->getResponse()->getStatusCode());
+            }
+        }
+    }
+
+    private function internalExecution()
+    {
+        $socketIoConnector = $this->getContainer()->get('socket_io_connector')->ensureConnection();
         $em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
         $webHook = $em->getRepository('AppBundle:WebHook')->findOneBy(['endpoint' => 'webhook_1']);
         $request = new Request();
@@ -33,6 +78,5 @@ class SimulateNotificationCommand extends ContainerAwareCommand
         $request->request->add(['param_1' => 'value_1', 'param_2' => 'value_2']);
         $request->setMethod('POST');
         $socketIoConnector->forwardNotification($webHook, $request);
-        $output->writeln('Sent!');
     }
 }
