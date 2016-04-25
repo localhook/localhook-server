36b8fb227c7891e78d7a2dd6494d4ac21bc9f83b
Lucas CHERIFI <lucas@cherifi.info>
2016-04-25 21:27:41 +0200

    Update tests

--- src/AppBundle/Tests/Controller/WebHookControllerTest.php
+++ src/AppBundle/Tests/Controller/WebHookControllerTest.php
@@ -2,54 +2,64 @@
 
 namespace AppBundle\Tests\Controller;
 
+use Doctrine\ORM\Tools\SchemaTool;
+use Nelmio\Alice\Fixtures;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class WebHookControllerTest extends WebTestCase
 {
-    /*
+    public static function dump($var)
+    {
+        fwrite(STDERR, print_r($var, true));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
+        // TODO Load fixtures
+        $em = static::$kernel->getContainer()->get('doctrine.orm.default_entity_manager');
+        $metaData = $em->getMetadataFactory()->getAllMetadata();
+        if (!empty($metaData)) {
+            $tool = new SchemaTool($em);
+            $tool->dropSchema($metaData);
+            $tool->createSchema($metaData);
+            Fixtures::load(static::$kernel->getRootDir() . '/../src/AppBundle/DataFixtures/ORM/fixtures.yml', $em, ['providers' => [$this]]);
+        }
+    }
+
     public function testCompleteScenario()
     {
         // Create a new client to browse the application
-        $client = static::createClient();
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW'   => 'admin',
+        ]);
 
         // Create a new entry in the database
         $crawler = $client->request('GET', '/webhook/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /webhook/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
+        $this->assertEquals(200, $client->getResponse()
+                                        ->getStatusCode(), "Unexpected HTTP status code for GET /webhook/");
+        $crawler = $client->click($crawler->selectLink('Create a new webhook')->link());
 
         // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'appbundle_webhook[field_name]'  => 'Test',
-            // ... other fields to fill
-        ));
+        $form = $crawler->selectButton('Submit')->form([
+            'web_hook[endpoint]' => 'webhook_test',
+        ]);
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
-
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
-
-        $form = $crawler->selectButton('Update')->form(array(
-            'appbundle_webhook[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
+        $this->assertGreaterThan(0, $crawler->filter('a:contains("webhook_test")')
+                                            ->count(), 'Missing element a:contains("webhook_test")');
         // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
+        $client->submit($crawler->selectButton('Delete webhook_test')->form());
         $crawler = $client->followRedirect();
 
         // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $this->assertNotRegExp('/webhook_test/', $client->getResponse()->getContent());
     }
-
-    */
 }
