22d6ef0a91ba85a22c314336d5fcfd4c39cf11cb
Lucas CHERIFI <lucas@cherifi.info>
2016-05-05 23:06:18 +0200

    code style

--- src/AppBundle/UserBundle/EventListener/RegistrationInitializeListener.php
+++ src/AppBundle/UserBundle/EventListener/RegistrationInitializeListener.php
@@ -6,7 +6,6 @@ use AppBundle\Entity\User;
 use FOS\UserBundle\Event\GetResponseUserEvent;
 use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Listener responsible to change the redirection at the end of the password resetting
